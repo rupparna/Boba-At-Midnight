37da5bab83d2d218dbfc5b3e8a3bf252a507a32b
rupparna <rupparna3@gmail.com>
2026-08-15 12:17:18 +0530

    debugging

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
    
+
 const fortunes = [
   "A spontaneous adventure will find you tonight. Say yes!",
   "Beware of extra toppings... they might steal your heart (or your appetite).",
@@ -10,6 +11,18 @@ const fortunes = [
   "Great joy will come from a very small snack."
 ];
 
+
+const fortuneText = document.getElementById('fortune-text');
+const fortuneBtn = document.getElementById('fortune-btn');
+
+
+fortuneBtn.addEventListener('click', () => {
+ 
+  const randomIndex = Math.floor(Math.random() * fortunes.length);
+  
+
+  fortuneText.textContent = fortunes[randomIndex];
+});
 
 
 const bgMusic = new Audio('./leberch-creepy-spooky-piano-262593.mp3');
